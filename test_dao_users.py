6f8ef2b361f31a1c0d47048ad9e28005cfbec802
Blent User <user@blent>
2026-05-22 15:29:58 +0000

    pytest test_dao_users.py

---
+++ test_dao_users.py
@@ -0,0 +1,55 @@
+import pytest
+
+from dao_users import create_user
+
+
+def create_user(user):
+    if user.__class__.__name__ == 'User':
+        if len(user.id) > 0 and len(user.password) > 0:
+            if (user.client and not user.administrator) or (user.administrator and not user.client):
+                try:
+                    db.session.query(User).filter_by(id=user.id).one()
+                    raise ValueError("L'utilisateur existe déjà en base de donnée.")
+                except NoResultFound as e:
+                    # Ajouter à la session
+                    db.session.add(user)
+                    db.session.commit()
+            else:
+                raise ValueError("Soit l'utilisateur est client, soit il est administrateur.")
+        else:
+            raise ValueError("L'identifiant et le mot de passe doivent être renseigné.")
+    else:
+        raise ValueError("L'utilisateur n'est pas valide.")
+
+def test_not_user():
+    with pytest.raises(ValueError("L'utilisateur n'est pas valide.")):
+        create_user(CartItem(id=1,cart_id=1,product_id=1,quantity=2))
+
+
+@pytest.mark.parametrize(
+    ["x", "y", "expectation"],
+    [
+        ("",  "2", raises(ValueError("L'identifiant et le mot de passe doivent être renseigné."))),
+        ("r", "", raises(ValueError("L'identifiant et le mot de passe doivent être renseigné.")))
+        
+    ],
+)
+def test_identifiant_not_provided(x, y, expectation):
+    with expectation:
+        create_user(User(id=x,password=y,client=True,administrator=False))
+
+@pytest.mark.parametrize(
+    ["x", "y", "expectation"],
+    [
+        (False,  False, raises(ValueError("Soit l'utilisateur est client, soit il est administrateur."))),
+        (True, True, raises(ValueError("Soit l'utilisateur est client, soit il est administrateur.")))
+        
+    ],
+)
+def test_client_or_administrator(x, y, expectation):
+    with expectation:
+        create_user(User(id="admin",password="password",client=x,administrator=y))
+
+def test_user_is_created():
+    create_user(User(id="",password=y,client=True,administrator=False))
+    assert calc.add(2, 3) == 5
